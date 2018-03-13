4cd24e0887e86fc4f69429f3d791fc252ff39357
Git <nico@hipermediaz.com>
2018-03-13 14:48:35 +0100

    Agregar archivos de proyecto.

---
+++ ataria/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ataria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mainin = 0;
+        Main:
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("MARIANISTAS - Pagina del Alumno ");
+            Console.WriteLine("Debug: (" + mainin + ")");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("     (1) Google[MENU]");
+            Console.WriteLine("     (2) Educamos");
+            Console.WriteLine("     (3) Burlington Books");
+            Console.WriteLine("     (4) SmSaviaDigital");
+            Console.WriteLine("");
+            Console.WriteLine("     (5) Apagar Ordenador");
+            Console.WriteLine("     (6) Salir");
+            Console.WriteLine("");
+            Console.WriteLine("Donde desea entrar? (1/2/3/4/5/6):");
+            mainin = Int32.Parse(Console.ReadLine());
+
+
+            switch (mainin)
+            {
+                case 2:
+                    System.Diagnostics.Process.Start("http://marias-gasteiz.educamos.com/");
+                    break;
+
+                default:
+                    Console.WriteLine("");
+                    Console.WriteLine("Typed /" + mainin + "/");
+                    Console.WriteLine();
+                    break;
+            }
+            Console.Clear();
+            goto Main;
+        }
+    }
+}
